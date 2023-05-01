d9d541c54fe34ecdd78beed62b293da4a1aedede
AlexisBeltran <jesus_beltran011@outlook.com>
2023-04-30 22:02:22 -0700

    API REST - CROSSFIT

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//ADD
+app.use(bodyParser.json());
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+    console.log(`API IS LISTENING ON PORT ${PORT}`);
+    V1SwaggerDocs(app, PORT);
+
+});
