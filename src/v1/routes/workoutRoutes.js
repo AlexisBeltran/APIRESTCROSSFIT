0f7ea86a1d6cb9fb4fe93e5405287ccad99e95d1
AlexisBeltran <jesus_beltran011@outlook.com>
2023-04-30 22:34:03 -0700

    Doc api

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -47,6 +47,196 @@ const workoutControllers = require("../../controllers/workoutController");
  *                       example: "Some error message"
  */
 
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the workout to retrieve
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: NOT FOUND
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     tags:
+ *       - Workouts
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Workout"
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: CREATED
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: BAD REQUEST
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Invalid workout data"
+ */
+
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   patch:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the workout to update
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/WorkoutUpdate"
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   $ref: "#/components/schemas/Workout"
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: BAD REQUEST
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Invalid workout update data"
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: NOT FOUND
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+
+
+/**
+ * @openapi
+ * /api/v1/workouts/{id}:
+ *   delete:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The ID of the workout to delete
+ *     responses:
+ *       204:
+ *         description: No Content
+ *       404:
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: NOT FOUND
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Workout not found"
+ */
+
+
 router.get("/", workoutControllers.getAllWorkouts);
 
 router.get("/:workoutId", workoutControllers.getOneWorkout);
